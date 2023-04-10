c0bdaab4688cd188e9eeb17d8dc241339a64610e
Genn <genesismaldonadoaugusto@gmail.com>
2023-04-09 23:44:36 -0400

    profile slide mostly done + read me
    Read me file was started not finished, and profile slide is mostly done needs some tweaks for alignment within the post content.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Devami
-MissCoding Devami
+
+## Table of contents
+
+- Description of the project
+- Collaborators
+- Built With
+- Screenshot of propotype
+- Struggles
+-License
+
+## Description
+
+We have a MPV for a website that is design for web-developer to connect and network as well as learn.
+
+## Collaborators
+
+
+
+
+
+## Built With:
+
+- JavaScript
+- Node
+- NPM
+- Webpack
+- HTML
+- CSS
+
+
+## Screenshot of prototype:
+
+
+
+## Struggles:
+
+
+## Wins:
+
+
+
+## License:
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-    $('#post-slider').carousel({
-      interval: false
-    });
-  
-    $('#post-slider .left carousel-control').click(function(){
-      $('#post-slider').carousel('prev');
-    });
-  
-    $('#post-slider .right carousel-control').click(function(){
-      $('#post-slider').carousel('next');
-    });
-  });
+var carousel = document.getElementById('post-carousel');
+var wrapper = carousel.querySelector('.carousel-wrapper');
+var prev = carousel.querySelector('.carousel-prev');
+var next = carousel.querySelector('.carousel-next');
+var items = carousel.querySelectorAll('.carousel-item');
+var slideWidth = items[0].offsetWidth;
+var currentIndex = 0;
+
+function slideTo(index) {
+  wrapper.style.transform = 'translateX(-' + (slideWidth * index) + 'px)';
+  currentIndex = index;
+}
+
+prev.addEventListener('click', function() {
+  if (currentIndex > 0) {
+    slideTo(currentIndex - 1);
+  }
+});
+
+next.addEventListener('click', function() {
+  if (currentIndex < items.length - 3) {
+    slideTo(currentIndex + 1);
+  }
+});
+
+slideTo(0);
